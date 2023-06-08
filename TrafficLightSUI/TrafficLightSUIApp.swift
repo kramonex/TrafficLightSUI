342b496284edc48c7a5457f1bd34c4511f45f664
kramonex <kramonex1@yandex.ru>
2023-06-08 18:31:17 +0300

    Initial Commit

---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by kramonex on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
